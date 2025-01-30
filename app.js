72379cd5a3849f54c0159690c8eaba962b6e2261
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2025-01-30 23:24:17 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let start= document.getElementById("start")
+let stop= document.getElementById("stop")
+let reset= document.getElementById("reset")
+let reset= document.getElementById("reset")
+let counter=0
+let interval;
+let firstCall=false
