7ab68749e0344b2c01a4bcf25c825da957539dc9
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2025-01-30 23:58:28 +0500

    done

--- app.js
+++ app.js
@@ -1,7 +1,65 @@
-let start= document.getElementById("start")
-let stop= document.getElementById("stop")
-let reset= document.getElementById("reset")
-let reset= document.getElementById("reset")
-let counter=0
+// let start= document.getElementById("start")
+// let stopWatch= document.getElementById("stop")
+// let reset= document.getElementById("reset")
+// let display= document.getElementById("display")
+// let counter=0
+// let interval;
+// let min=0;
+// start.addEventListener('click', function(){
+//     interval= setInterval(function(){
+//         counter++
+//         if(counter>3){
+//        min++
+//        counter=0
+//     }
+//         display.innerHTML=min
+//         display.innerHTML=counter
+//     },1000)
+//     start.disabled=true
+//     stopWatch.disabled=false
+//     stopWatch.addEventListener('click',function(){
+//         clearInterval(interval)
+//         start.disabled=false
+//         stopWatch.disabled=true
+//     })
+//     reset.addEventListener('click',function(){
+//         clearInterval(interval)
+//         counter=0
+//         display.innerHTML =counter
+//     })
+// })
+let start = document.getElementById("start");
+let stopWatch = document.getElementById("stop");
+let reset = document.getElementById("reset");
+let display = document.getElementById("display");
+let counter = 0;
 let interval;
-let firstCall=false
+let min = 0;
+
+start.addEventListener('click', function () {
+    interval = setInterval(function () {
+        counter++;
+        if (counter >= 60) { 
+            min++;           
+            counter = 0; 
+        }
+        display.innerHTML = `${min}:${counter}`; // Display minutes and seconds
+    }, 1000);
+    start.disabled = true;
+    stopWatch.disabled = false;
+
+    stopWatch.addEventListener('click', function () {
+        clearInterval(interval);
+        start.disabled = false;
+        stopWatch.disabled = true;
+    });
+
+    reset.addEventListener('click', function () {
+        clearInterval(interval);
+        counter = 0;
+        min = 0;
+        display.innerHTML = `0:00`; 
+        start.disabled = false;
+        stopWatch.disabled = true;
+    });
+});
