577ad65e95db7c152db6928246344ef21e786cc8
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2025-01-31 00:00:46 +0500

    remvd

--- app.js
+++ app.js
@@ -1,33 +1,4 @@
-// let start= document.getElementById("start")
-// let stopWatch= document.getElementById("stop")
-// let reset= document.getElementById("reset")
-// let display= document.getElementById("display")
-// let counter=0
-// let interval;
-// let min=0;
-// start.addEventListener('click', function(){
-//     interval= setInterval(function(){
-//         counter++
-//         if(counter>3){
-//        min++
-//        counter=0
-//     }
-//         display.innerHTML=min
-//         display.innerHTML=counter
-//     },1000)
-//     start.disabled=true
-//     stopWatch.disabled=false
-//     stopWatch.addEventListener('click',function(){
-//         clearInterval(interval)
-//         start.disabled=false
-//         stopWatch.disabled=true
-//     })
-//     reset.addEventListener('click',function(){
-//         clearInterval(interval)
-//         counter=0
-//         display.innerHTML =counter
-//     })
-// })
+
 let start = document.getElementById("start");
 let stopWatch = document.getElementById("stop");
 let reset = document.getElementById("reset");
@@ -43,7 +14,7 @@ start.addEventListener('click', function () {
             min++;           
             counter = 0; 
         }
-        display.innerHTML = `${min}:${counter}`; // Display minutes and seconds
+        display.innerHTML = `${min}:${counter}`; 
     }, 1000);
     start.disabled = true;
     stopWatch.disabled = false;
